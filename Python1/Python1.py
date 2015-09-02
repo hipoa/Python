8d52e9fc61f8ed2475ca1dc7aecbe214edc9094e
hipoa <lockband@naver.com>
2015-09-02 13:48:06 +0900

    파이썬프로그래밍
    2015.09.02 lec1

---
+++ Python1/Python1.py
@@ -0,0 +1,27 @@
+#program example 1
+#salary = int(input("월급: "))
+#bonus = int(input("보너스: "))
+#payCheck = salary + bonus
+#print(payCheck)
+#print(type(payCheck)) #타입을 확인하는 방법
+
+#주석처리 : ctr +kc  ,  해제 : ctr + ku
+
+#name = "junha"
+#print(name[-2])
+
+#info = "201111291junha"
+#sid = info[:9]
+#sname = info[9:]
+#print(sid + " " + sname)
+#a = "I eat %10s apples." % "five"
+#b = "Error is %d%%." %98
+#print(a)
+#print(b)
+
+a = ","
+print(a.join("abcd"))
+
+answer = input("종료할까요? ")
+
+print(answer.upper())
